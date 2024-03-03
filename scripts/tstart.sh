bedd18c2b0120234de6c2451ec5eb04d990cb90c
SparrowMike <michal.m_wroblewski@hotmail.com>
2024-03-03 18:55:39 +0800

    fix: 🐛 not triggering on first session, new widnow on if

--- scripts/tstart.sh
+++ scripts/tstart.sh
@@ -7,7 +7,18 @@ tmux rename-session "$session"
 tmux rename-window -t "$session":"$window" "editor"
 tmux send-keys -t "$session":"$window" 'nvim' C-m
 
-if [ "$session" != "tmux" ] && [ "$session" != "nvim" ]; then
+shopt -s nullglob  
+
+sh_files=( *.sh )
+lua_files=( *.lua )
+tmux_files=( *.tmux )
+
+count_files=$(( ${#sh_files[@]} + ${#lua_files[@]} + ${#tmux_files[@]} ))
+
+# --- alternative to above --- 
+# count_files=$(find . -maxdepth 1 -type f \( -name "*.sh" -o -name "*.lua" -o -name "*.tmux" \) | wc -l)
+
+if [ "$count_files" -eq 0 ]; then
     window=2
     tmux new-window -t "$session":"$window" -n 'terminal'
     tmux send-keys -t "$session":"$window" 'npm run dev' C-m
@@ -16,4 +27,3 @@ fi
 tmux select-window -t "$session":"1"
 
 exit 0
-
