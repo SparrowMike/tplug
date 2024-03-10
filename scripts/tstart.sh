574b2b29c726eb996b8bdeff1dde1b7b1f05d08e
SparrowMike <michal.m_wroblewski@hotmail.com>
2024-03-10 22:11:30 +0800

    refactor: 💡 create second window only on js/ts files

--- scripts/tstart.sh
+++ scripts/tstart.sh
@@ -9,16 +9,21 @@ tmux send-keys -t "$session":"$window" 'nvim' C-m
 
 shopt -s nullglob  
 
-sh_files=( *.sh )
-lua_files=( *.lua )
-tmux_files=( *.tmux )
-
-count_files=$(( ${#sh_files[@]} + ${#lua_files[@]} + ${#tmux_files[@]} ))
-
+# sh_files=( *.sh )
+# lua_files=( *.lua )
+# tmux_files=( *.tmux )
+#
+# count_files=$(( ${#sh_files[@]} + ${#lua_files[@]} + ${#tmux_files[@]} ))
+#
 # --- alternative to above --- 
 # count_files=$(find . -maxdepth 1 -type f \( -name "*.sh" -o -name "*.lua" -o -name "*.tmux" \) | wc -l)
 
-if [ "$count_files" -eq 0 ]; then
+js_files=( *.js )
+ts_files=( *.ts )
+
+count_files=$(( ${#js_files[@]} + ${#ts_files[@]} ))
+
+if [ "$count_files" -gt 0 ]; then
     window=2
     tmux new-window -t "$session":"$window" -n 'terminal'
     tmux send-keys -t "$session":"$window" 'npm run dev' C-m
